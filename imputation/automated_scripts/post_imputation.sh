853cadc800398ac01e242716400542b08d44a97c
Silvia Diz <61839435+silviaadiz@users.noreply.github.com>
2025-07-29 10:21:53 +0200

    Update post_imputation.sh
    X vcf file error

--- imputation/automated_scripts/post_imputation.sh
+++ imputation/automated_scripts/post_imputation.sh
@@ -24,8 +24,14 @@ unzip -P ${PASSWORD} \*
 
 
 for i in {1..23}; do
+    if ["$i" -eq 23]; then
+        VCF_FILE="chrX.dose.vcf.gz"
+    else
+        VCF_FILE="chr${i}.dose.vcf.gz"
+    fi
+
     $STORE2/plink/plink2 \
-        --vcf "${DIR_IMPUTACION}/chr${i}.dose.vcf.gz" \
+        --vcf "${DIR_IMPUTACION}/${VCF_FILE}" \
         --set-all-var-ids 'chr@:#:$r:$a' \
         --extract-if-info "R2>=0.8" \
         --new-id-max-allele-len 100 \
@@ -34,7 +40,6 @@ for i in {1..23}; do
         --make-bed \
         --out "${DIR_IMPUTACION}/${PREFIX_IMPUTADOS}_chr${i}_post_qc_paso01"
 done
-
 #----------------------------------------------------------------------------------------------------------------------------------
 # Paso 3: Reemprazo do arquivo FAM co orixinal pre-imputación
 # NOTA: COMPROBAR QUE O CHR IMPUTADO CORRESPONDE AOS INDIVIDUOS XENOTIPADOS, é dicir, que sexan OS MESMOS INDIVIDUOS E NA MESMA ORDE

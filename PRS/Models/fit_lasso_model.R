dee1c3a67b7fde07f094a82f4942565da89625a8
Silvia Diz <61839435+silviaadiz@users.noreply.github.com>
2025-08-11 09:24:38 +0200

    Update fit_lasso_model.R

--- PRS/Models/fit_lasso_model.R
+++ PRS/Models/fit_lasso_model.R
@@ -1,87 +1,91 @@
-
 library(glmnet)
 library(pROC)
 
 # First, we compute the AUC corrected for optimism:
 
-boot_lasso <- function(data, pheno, covar, interactions = FALSE, interactions_covar = NULL, B) {
+boot_lasso <- function(data, pheno, covar, interactions = FALSE, interactions_covar = NULL, B = 1000) {
   set.seed(1711)
-
+  
   # Filter rows with missing values (pending imputation)
   dat_na <- data[complete.cases(data[, c(as.character(pheno), as.character(covar))]), , drop = FALSE]
- 
-  # Build model formula
+  
+  # Model formula
   if (interactions && !is.null(interactions_covar)) {
-    f1 <- as.formula(sprintf('%s ~ %s + %s', as.character(pheno), paste(covar, collapse = "+"), paste(interactions_covar, collapse = "+")))
-  } else if (!interactions) {
-    f1 <- as.formula(sprintf('%s ~ %s', as.character(pheno), paste(covar, collapse = "+")))
+    f1 <- as.formula(sprintf(
+      '%s ~ %s + %s', 
+      as.character(pheno), 
+      paste(covar, collapse = " + "), 
+      paste(interactions_covar, collapse = " + ")
+    ))
+  } else {
+    f1 <- as.formula(sprintf(
+      '%s ~ %s', 
+      as.character(pheno), 
+      paste(covar, collapse = " + ")
+    ))
   }
- 
+  
   # Create model matrix 
   x <- model.matrix(f1, data = dat_na)
   n <- nrow(dat_na)
- 
+  
   # Initialize matrices for results
-  coef_bootstrap_1se <- matrix(NA, nrow = B, ncol = ncol(x) + 1)
-  colnames(coef_bootstrap_1se) <- c("intercept", colnames(x))
-
-  boot.lambda <- matrix(NA, nrow = B, ncol = 1)
-  colnames(boot.lambda) <- c("1se")
- 
-  preds <- matrix(NA, nrow = nrow(dat_na), ncol = B)
-  colnames(preds) <- seq(1, B, 1)
- 
-  optimism <- matrix(NA, nrow = B, ncol = 1)  
- 
+  coef_bootstrap_1se <- matrix(NA, nrow = B, ncol = ncol(x))
+  colnames(coef_bootstrap_1se) <- colnames(x)
+  
+  boot_lambda <- numeric(B)
+  
+  preds <- matrix(NA, nrow = n, ncol = B)
+  colnames(preds) <- paste0("Boot_", seq_len(B))
+  
+  optimism <- numeric(B)
+  
   # Train full model
-  boot_cv_lasso_full <- cv.glmnet(x = x, y = dat_na[, pheno], alpha = 1)
-  full_model_auc <- auc(roc(dat_na[, pheno], predict(boot_cv_lasso_full, newx = x, s = boot_cv_lasso_full$lambda.1se)))
- 
+  boot_cv_lasso_full <- cv.glmnet(x = x, y = dat_na[, pheno], alpha = 1, family = "binomial")
+  full_model_pred <- predict(boot_cv_lasso_full, newx = x, s = "lambda.1se", type = "response")
+  full_model_auc <- auc(roc(dat_na[, pheno], as.vector(full_model_pred)))
+  
   # Bootstrapping
-  for (i in 1:B) {
-    ind <- sample(1:n, size = n, replace = TRUE)  
-    x_boot <- x[ind, ]
+  for (i in seq_len(B)) {
+    ind <- sample(seq_len(n), size = n, replace = TRUE)
+    x_boot <- x[ind, , drop = FALSE]
     y_boot <- dat_na[ind, pheno]
-   
-    boot_cv_lasso <- cv.glmnet(x = x_boot, y = y_boot, alpha = 1)
-   
+    
+    boot_cv_lasso <- cv.glmnet(x = x_boot, y = y_boot, alpha = 1, family = "binomial")
+    
     # Now we retrieve coefficients for the best lambda
-    coef_bootstrap_1se[i, ] <- as.numeric(coef(boot_cv_lasso$glmnet.fit, s = boot_cv_lasso$lambda.1se))
-    boot.lambda[i, ] <- boot_cv_lasso$lambda.1se
-   
+    coef_bootstrap_1se[i, ] <- as.numeric(coef(boot_cv_lasso, s = "lambda.1se"))
+    boot_lambda[i] <- boot_cv_lasso$lambda.1se
+    
     # Predict in bootstrap resample
-    preds_boot <- predict(boot_cv_lasso, newx = x_boot, type = "response", s = "lambda.1se")
-   
+    preds_boot <- predict(boot_cv_lasso, newx = x_boot, s = "lambda.1se", type = "response")
+    auc_boot <- auc(roc(y_boot, as.vector(preds_boot)))
+    
     # Predict in test data (full sample)
-    preds[, i] <- predict(boot_cv_lasso, newx = x, type = "response", s = "lambda.1se")
-   
-    # Compute AUC
-    roc_curve_boot <- auc(roc(y_boot, as.vector(preds_boot)))
-   
-    roc_curve_test <- auc(roc(dat_na[, pheno], as.vector(preds[, i])))
-   
+    preds_full <- predict(boot_cv_lasso, newx = x, s = "lambda.1se", type = "response")
+    preds[, i] <- preds_full
+    auc_test <- auc(roc(dat_na[, pheno], as.vector(preds_full)))
+    
     # Compute optimism for this resample
-    optimism[i, ] <- roc_curve_boot - roc_curve_test
+    optimism[i] <- auc_boot - auc_test
   }
- 
-  # Last, we compute the AUC for the model corrected for optimism
+  
+   # Lastly, we compute the AUC for the model corrected for optimism
   auc_opt_cor_avg <- full_model_auc - mean(optimism)
- 
-  prob.nonzero.1se <- apply(abs(coef_bootstrap_1se) > 1e-5, 2, mean)
- 
+
+  prob_nonzero_1se <- colMeans(abs(coef_bootstrap_1se) > 1e-5)
+  
   # Final results
   res <- list(
-    coef = coef_bootstrap_1se,  
-    lambda = boot.lambda,
-    prob.nonzero = prob.nonzero.1se,
+    coef = coef_bootstrap_1se,
+    lambda = boot_lambda,
+    prob_nonzero = prob_nonzero_1se,
     pred_cal = preds,
-    optimism = optimism,  
-    opt_cor_auc_promediado = auc_opt_cor_avg,  
-    full_model_auc = full_model_auc  
+    optimism = optimism,
+    opt_cor_auc_promediado = auc_opt_cor_avg,
+    full_model_auc = full_model_auc
   )
- 
+  
   return(res)
 }
-
-
-# Second: we would fit the full model and report the corrected AUC                  
+     

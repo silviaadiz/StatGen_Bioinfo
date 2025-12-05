f84071704a1085c1a327c3d1c23cd3a53a8f11ab
Silvia Diz <61839435+silviaadiz@users.noreply.github.com>
2025-12-05 12:54:12 +0100

    Update 2_Convertir_rsid_a_SNP.sh

--- PRS/PRS-cs_PRS-csx/prs_without_rsid/2_Convertir_rsid_a_SNP.sh
+++ PRS/PRS-cs_PRS-csx/prs_without_rsid/2_Convertir_rsid_a_SNP.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+PHENO_NAME="blabla"
+SCOREFILE_OUTPUT="nome_para_scorefile"
+TARGET_NAME="nome_cohorte_individuos_target_para_output"
 
-cat /ruta/a/scores/_pst_eff_a1_b0.5_phiauto_chr* > scores_PRScs_SA_Doch_EUR.prscs_to_EBeur.and.GIULIA_allchr.txt
 
-Rscript --vanilla convert_rsid_a_SNP.R
+cat /ruta/a/scores/PRS_${PHENO_NAME}_pst_eff_a1_b0.5_phiauto_chr* > "${SCOREFILE_OUTPUT}"
 
-cut -f2 -d$'\t' "SNP.RSID.Scores_prscs_SA.Doch.EUR_to_EBeur.and.GIULIA.txt" > extract_snps_prscs_SA.Doch.EUR_to_EBeur.and.GIULIA.txt
+Rscript --vanilla convertir_rsid_a_SNP.R \
+--equivalence_file "/ruta/a/equiv.file.rs.snp/.txt" --score "${SCOREFILE_OUTPUT}" --pheno_name ${PHENO_NAME} --target_name ${TARGET_NAME}
+  
+cut -f2 -d$'\t' "SNP.RSID.Scores_prscs_${PHENO_NAME}_to_${TARGET_NAME}.txt" > "extract_snps_prscs_${PHENO_NAME}_to_${TARGET_NAME}.txt"
